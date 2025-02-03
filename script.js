7b0de3e5c40d8a2d213dbee0174d0f399ec1751b
Alex <86182397+alehxs@users.noreply.github.com>
2025-02-02 21:52:03 -0600

    Inital commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('link-button');
+
+// Link to Github
+  button.addEventListener('click', () => {
+    window.location.href = 'https://github.com/yourusername'; // Change this to your GitHub or LinkedIn URL
+  });
+});
